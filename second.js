905d1fe7ffaa401cbc6fdaa783b48dceae5b0f1c
kublashvilidachi <130204826+kublashvilidachi@users.noreply.github.com>
2023-04-21 00:06:59 +0400

    Add files via upload

---
+++ second.js
@@ -0,0 +1,29 @@
+let currentCar = 1
+let car = document.querySelector("#cars")
+function changeCar1(){
+    car.src = "images/audi1.jpg"
+}
+function changeCar2(){
+    car.src = "images/audi2.jpg"
+}
+function changeCar3(){
+    car.src = "images/audi3.jpg"
+}
+function changeCar4(){
+    car.src = "images/audi4.png"
+}
+function changeCar5(){
+    car.src = "images/audi5.png"
+}
+function changeCar6(){
+    car.src = "images/audi7.png"
+}
+function changeCar7(){
+    car.src = "images/audi6.png"
+}
+function changeCar8(){
+    car.src = "images/audi8.png"
+}
+function changeCar9(){
+    car.src = "images/audi9.png"
+}
