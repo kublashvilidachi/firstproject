f3f33ee5744bffa8a43d1a6622971571a6d84c8f
kublashvilidachi <130204826+kublashvilidachi@users.noreply.github.com>
2023-04-23 23:03:38 +0400

    Add files via upload

--- login.js
+++ login.js
@@ -2,10 +2,20 @@ let error = document.querySelector("#error")
 let username = document.querySelector("#username")
 let password = document.querySelector("#password")
 let maindiv = document.querySelector("#maindiv")
+let mail = document.querySelector("#mail")
 
 function eror(){
     error.style.display = "block"
     username.value = ""
     password.value = ""
     maindiv.style.border = "solid 1px red"
+    maindiv.style.boxShadow = "0px 0px 8px red"
+}
+function regeror(){
+    error.style.display = "block"
+    username.value = ""
+    password.value = ""
+    maindiv.style.border = "solid 1px red"
+    mail.value = ""
+    maindiv.style.boxShadow = "0px 0px 8px red"
 }
