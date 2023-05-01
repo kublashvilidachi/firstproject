cfc76fe9aeb3d7334be6ff7f35f188e600531e21
kublashvilidachi <130204826+kublashvilidachi@users.noreply.github.com>
2023-05-01 21:23:25 +0400

    Add files via upload

---
+++ models.js
@@ -0,0 +1,304 @@
+let change = document.querySelector("#changebtn")
+let archevani = document.querySelector("#changecolor")
+let num = "1"
+let rigi = document.querySelector("#rigi")
+
+function gaqroba(){
+    archevani.style.display = "none"
+}
+function gachena(){
+    archevani.style.display = "block"
+}
+
+let image1 = "images/poto-1color-1.png"
+let image2 = "images/poto-2color-1.png"
+let image3 = "images/poto-3color-1.png"
+let image4 = "images/poto-4color-1.png"
+let image5 = "images/poto-5color-1.png"
+let image6 = "images/poto-6color-1.png"
+let image7 = "images/poto-7color-1.png"
+let image8 = "images/poto-8color-1.png"
+
+
+let peri = "white"
+
+function clickedon1(){
+    peri = "white"
+    image1 = "images/poto-1color-1.png"
+    image2 = "images/poto-2color-1.png"
+    image3 = "images/poto-3color-1.png"
+    image4 = "images/poto-4color-1.png"
+    image5 = "images/poto-5color-1.png"
+    image6 = "images/poto-6color-1.png"
+    image7 = "images/poto-7color-1.png"
+    image8 = "images/poto-8color-1.png"
+
+    img.style.background = `url( "images/poto-${num}color-1.png" )`
+    img.style.backgroundRepeat = 'no-repeat'
+    img.style.backgroundPosition = 'center'
+}
+function clickedon2(){
+    peri = "black"
+    image1 = "images/poto-1color-2.png"
+    image2 = "images/poto-2color-2.png"
+    image3 = "images/poto-3color-2.png"
+    image4 = "images/poto-4color-2.png"
+    image5 = "images/poto-5color-2.png"
+    image6 = "images/poto-6color-2.png"
+    image7 = "images/poto-7color-2.png"
+    image8 = "images/poto-8color-2.png"
+
+    img.style.background = `url( "images/poto-${num}color-2.png" )`
+    img.style.backgroundRepeat = 'no-repeat'
+    img.style.backgroundPosition = 'center'
+}
+function clickedon3(){
+    peri = "darkgray"
+    peri = "white"
+    image1 = "images/poto-1color-3.png"
+    image2 = "images/poto-2color-3.png"
+    image3 = "images/poto-3color-3.png"
+    image4 = "images/poto-4color-3.png"
+    image5 = "images/poto-5color-3.png"
+    image6 = "images/poto-6color-3.png"
+    image7 = "images/poto-7color-3.png"
+    image8 = "images/poto-8color-3.png"
+
+    img.style.background = `url( "images/poto-${num}color-3.png" )`
+    img.style.backgroundRepeat = 'no-repeat'
+    img.style.backgroundPosition = 'center'
+}
+function clickedon4(){ 
+    peri = "normalgray"
+    peri = "white"
+    image1 = "images/poto-1color-4.png"
+    image2 = "images/poto-2color-4.png"
+    image3 = "images/poto-3color-4.png"
+    image4 = "images/poto-4color-4.png"
+    image5 = "images/poto-5color-4.png"
+    image6 = "images/poto-6color-4.png"
+    image7 = "images/poto-7color-4.png"
+    image8 = "images/poto-8color-4.png"
+
+    img.style.background = `url( "images/poto-${num}color-4.png" )`
+    img.style.backgroundRepeat = 'no-repeat'
+    img.style.backgroundPosition = 'center'
+}
+function clickedon5(){
+    if(peri = String){
+    peri = "lightgray"
+    }
+    peri = "white"
+    image1 = "images/poto-1color-5.png"
+    image2 = "images/poto-2color-5.png"
+    image3 = "images/poto-3color-5.png"
+    image4 = "images/poto-4color-5.png"
+    image5 = "images/poto-5color-5.png"
+    image6 = "images/poto-6color-5.png"
+    image7 = "images/poto-7color-5.png"
+    image8 = "images/poto-8color-5.png"
+
+    img.style.background = `url( "images/poto-${num}color-5.png" )`
+    img.style.backgroundRepeat = 'no-repeat'
+    img.style.backgroundPosition = 'center'
+}
+function clickedon6(){
+    if(peri = String){
+    peri = "silver"
+    }
+    peri = "white"
+    image1 = "images/poto-1color-6.png"
+    image2 = "images/poto-2color-6.png"
+    image3 = "images/poto-3color-6.png"
+    image4 = "images/poto-4color-6.png"
+    image5 = "images/poto-5color-6.png"
+    image6 = "images/poto-6color-6.png"
+    image7 = "images/poto-7color-5.png"
+    image8 = "images/poto-8color-6.png"
+
+    img.style.background = `url( "images/poto-${num}color-6.png" )`
+    img.style.backgroundRepeat = 'no-repeat'
+    img.style.backgroundPosition = 'center'
+}
+function clickedon7(){
+    if(peri = String){
+    peri = "blue"
+    }
+    peri = "white"
+    image1 = "images/poto-1color-7.png"
+    image2 = "images/poto-2color-7.png"
+    image3 = "images/poto-3color-7.png"
+    image4 = "images/poto-4color-7.png"
+    image5 = "images/poto-5color-7.png"
+    image6 = "images/poto-6color-7.png"
+    image7 = "images/poto-7color-7.png"
+    image8 = "images/poto-8color-7.png"
+
+    img.style.background = `url( "images/poto-${num}color-7.png" )`
+    img.style.backgroundRepeat = 'no-repeat'
+    img.style.backgroundPosition = 'center'
+}
+function clickedon8(){
+    if(peri = String){
+    peri = "red"
+    }
+    peri = "white"
+    image1 = "images/poto-1color-8.png"
+    image2 = "images/poto-2color-8.png"
+    image3 = "images/poto-3color-8.png"
+    image4 = "images/poto-4color-8.png"
+    image5 = "images/poto-5color-8.png"
+    image6 = "images/poto-6color-8.png"
+    image7 = "images/poto-7color-8.png"
+    image8 = "images/poto-8color-8.png"
+
+    img.style.background = `url( "images/poto-${num}color-8.png" )`
+    img.style.backgroundRepeat = 'no-repeat'
+    img.style.backgroundPosition = 'center'
+}
+function clickedon9(){
+    if(peri = String){
+        peri = "yellow"
+    }
+    peri = "white"
+    image1 = "images/poto-1color-9.png"
+    image2 = "images/poto-2color-9.png"
+    image3 = "images/poto-3color-9.png"
+    image4 = "images/poto-4color-9.png"
+    image5 = "images/poto-5color-9.png"
+    image6 = "images/poto-6color-9.png"
+    image7 = "images/poto-7color-9.png"
+    image8 = "images/poto-8color-9.png"
+
+    img.style.background = `url( "images/poto-${num}color-9.png" )`
+    img.style.backgroundRepeat = 'no-repeat'
+    img.style.backgroundPosition = 'center'
+}
+
+let img = document.querySelector("#image")
+
+ 
+
+
+function right(){
+    if(peri = "white"){
+        if(num == "1"){
+            img.style.background = "url('" + image2 + "')"
+            img.style.backgroundRepeat = 'no-repeat'
+            img.style.backgroundPosition = 'center'
+            num = "2"
+            return
+        }
+        if(num == "2"){
+            img.style.background = "url('" + image3 + "')"
+            img.style.backgroundRepeat = 'no-repeat'
+            img.style.backgroundPosition = 'center'
+            num = "3"
+            return
+        }
+        if(num == "3"){
+            img.style.background = "url('" + image4 + "')"
+            img.style.backgroundRepeat = 'no-repeat'
+            img.style.backgroundPosition = 'center'
+            num = "4"
+            return
+        }
+        if(num == "4"){
+            img.style.background = "url('" + image5 + "')"
+            img.style.backgroundRepeat = 'no-repeat'
+            img.style.backgroundPosition = 'center'
+            num = "5"
+            return
+        }
+        if(num == "5"){
+            img.style.background = "url('" + image6 + "')"
+            img.style.backgroundRepeat = 'no-repeat'
+            img.style.backgroundPosition = 'center'
+            num = "6"
+            return
+        }
+        if(num == "6"){
+            img.style.background = "url('" + image7 + "')"
+            img.style.backgroundRepeat = 'no-repeat'
+            img.style.backgroundPosition = 'center'
+            num = "7";
+            return
+        }
+        if(num == "7"){
+            img.style.background = "url('" + image8 + "')"
+            img.style.backgroundRepeat = 'no-repeat'
+            img.style.backgroundPosition = 'center'
+            num = "8"
+            return
+        }
+        if(num == "8"){
+            img.style.background = "url('" + image1 + "')"
+            img.style.backgroundRepeat = 'no-repeat'
+            img.style.backgroundPosition = 'center'
+            num = "1"
+            return
+        }
+    }
+}
+function left(){
+    if(num == "2"){
+        img.style.background = "url('" + image1 + "')"
+        img.style.backgroundRepeat = 'no-repeat'
+        img.style.backgroundPosition = 'center'
+        num = "1"
+        return
+    }
+    if(num == "3"){
+        img.style.background = "url('" + image2 + "')"
+        img.style.backgroundRepeat = 'no-repeat'
+        img.style.backgroundPosition = 'center'
+        num = "2"
+        return
+    }
+    if(num == "4"){
+        img.style.background = "url('" + image3 + "')"
+        img.style.backgroundRepeat = 'no-repeat'
+        img.style.backgroundPosition = 'center'
+        num = "3"
+        return
+    }
+    if(num == "4"){
+        img.style.background = "url('" + image4 + "')"
+        img.style.backgroundRepeat = 'no-repeat'
+        img.style.backgroundPosition = 'center'
+        num = "4"
+        return
+    }
+    if(num == "6"){
+        img.style.background = "url('" + image5 + "')"
+        img.style.backgroundRepeat = 'no-repeat'
+        img.style.backgroundPosition = 'center'
+        num = "5"
+        return
+    }
+    if(num == "7"){
+        img.style.background = "url('" + image6 + "')"
+        img.style.backgroundRepeat = 'no-repeat'
+        img.style.backgroundPosition = 'center'
+        num = "6";
+        return
+    }
+    if(num == "8"){
+        img.style.background = "url('" + image7 + "')"
+        img.style.backgroundRepeat = 'no-repeat'
+        img.style.backgroundPosition = 'center'
+        num = "7"
+        return
+    }
+    if(num == "1"){
+        img.style.background = "url('" + image8 + "')"
+        img.style.backgroundRepeat = 'no-repeat'
+        img.style.backgroundPosition = 'center'
+        num = "8"
+        return
+    }
+}
+
+window.addEventListener("click", function(){
+    rigi.innerHTML = `${num} / 8`
+})
